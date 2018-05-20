4c0f7f2128a709cbef75a08b8f7ece32f32fa8da
t_takano <cerberus1974@gmail.com>
2018-05-20 21:19:10 +0900

    Add String Extensions Method IsNullOrEmpty & IsNotNullOrEmpty

---
+++ src/Seasoning/StringExtensions.cs
@@ -0,0 +1,15 @@
+namespace Seasoning.StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return !value.IsNullOrEmpty();
+        }
+    }
+}
